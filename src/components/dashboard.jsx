69d659248da75af97dd75772604bf8b4b2c5633a
samueltanner <samuel.m.s.tanner@gmail.com>
2021-05-18 14:27:34 -0700

    adds user.jobs axios request

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,10 +1,45 @@
 import React, { Component } from "react";
+import Metric from "./metric";
+import JobBoard from "./jobBoard";
+import axios from "axios";
+
 class Dashboard extends Component {
-  state = {};
+  state = {
+    jobs: [],
+  };
+
+  getUserJobs = () => {
+    axios.get("http://localhost:3000/api/users/1").then((response) => {
+      this.setState({jobs: response.data.jobs});
+      console.log(response);
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>I am the dashboard</h1>
+        <div className="metric-zone">
+          <Metric />
+          <Metric />
+          <Metric />
+          <Metric />
+          <Metric />
+          <button onClick={this.getUserJobs()}>JOBS</button>
+        </div>
+        <hr />
+        <div className="job-zone">
+          <div className="job-board">
+            <JobBoard />
+            <p>{ this.state.jobs.map(job => job) }</p>
+          </div>
+          <div className="job-board">
+            <JobBoard />
+          </div>
+          <div className="job-board">
+            <JobBoard />
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/components/jobBoard.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class JobBoard extends Component {
+  state = {  }
+  render() { 
+    return ( <div>
+      <h1>I am a Job Board</h1>
+    </div> );
+  }
+}
+ 
+export default JobBoard;
