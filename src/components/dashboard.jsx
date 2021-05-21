86aa8b14fa733e791c39765bc88fed729bf70c2e
samueltanner <samuel.m.s.tanner@gmail.com>
2021-05-21 14:38:07 -0700

    updates header, reactive job board, and fixes in contact:

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -4,20 +4,19 @@ import Metric from "./metric";
 import JobBoard from "./jobBoard";
 import JobCreate from "./jobCreate";
 
-
-
-
 class Dashboard extends Component {
-  state = {
-    jobs: [],
-    statuses: ["Saved", "Draft", "Applied", "In Contact", "Interviewing", "Offered", "Denied"],
-  };
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobs: [],
+      statuses: ["Saved", "Draft", "Applied", "In Contact", "Interviewing", "Offered", "Denied"],
+      newJobs: ["job"],
+    };
+  }
 
   componentDidMount() {
     this.getUserJobs();
   }
-
 
   getUserJobs = () => {
     axios.get("http://localhost:3000/api/users/" + localStorage.getItem("user_id")).then((response) => {
@@ -26,28 +25,29 @@ class Dashboard extends Component {
       // console.log(this.state.jobs);
     });
   };
-
+  handleCallBack = (childData) => {
+    this.setState((state) => ({ jobs: [...state.jobs, childData] }));
+  };
   jobDataFilter = (status) => {
     // console.log(Object.values(status)[0])
-    return (
-      this.state.jobs.filter(job => job.status === Object.values(status)[0])
-    )
-  }
-  
+    return this.state.jobs.filter((job) => job.status === Object.values(status)[0]);
+  };
+
   showModal = () => {
-    this.setState({ showModal: true })
-  }
+    this.setState({ showModal: true });
+  };
 
   closeModal = () => {
-    this.setState({ showModal: false })
-}
+    this.setState({ showModal: false });
+  };
 
   render() {
+    // const {newJobs} = this.state.newJobs
     return (
       <div>
         <h1>I am the dashboard</h1>
-        <button onClick={ this.showModal }>Add a Job</button>
-        { (this.state.showModal) ? <JobCreate closeModal={ this.closeModal }/> : null }
+        <button onClick={this.showModal}>Add a Job</button>
+        {this.state.showModal ? <JobCreate parentCallBack={this.handleCallBack} closeModal={this.closeModal} /> : null}
         <div className="metric-zone">
           <Metric />
           <Metric />
@@ -62,7 +62,7 @@ class Dashboard extends Component {
               <div className="job-board" key={index}>
                 {/* <JobBoard jobData={this.jobStatusFilter()} /> */}
                 <h2>{status}</h2>
-                <JobBoard jobData={this.jobDataFilter({status})}/>
+                <JobBoard jobData={this.jobDataFilter({ status })} />
               </div>
             );
           })}
